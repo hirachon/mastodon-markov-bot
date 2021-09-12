b4ded9bb6f26ebc50f4de738383dd5137b40bd0d
hirachon <33413396+hirachon@users.noreply.github.com>
2021-09-12 14:45:50 +0900

    4アカウント対応

--- src/app.py
+++ src/app.py
@@ -16,18 +16,37 @@ config_ini.read('config.ini', encoding='utf-8')
 
 def worker():
     # 学習
-    domain = config_ini['read']['domain']
-    read_access_token = config_ini['read']['access_token']
+
+    domain1 = config_ini['read']['domain1']
+    read_access_token1 = config_ini['read']['access_token1']
+    domain2 = config_ini['read']['domain2']
+    read_access_token2 = config_ini['read']['access_token2']
+    domain3 = config_ini['read']['domain3']
+    read_access_token3 = config_ini['read']['access_token3']
+    domain4 = config_ini['read']['domain4']
+    read_access_token4 = config_ini['read']['access_token4']
+    
+    domain = config_ini['write']['domain']
     write_access_token = config_ini['write']['access_token']
 
-    account_info = mastodonTool.get_account_info(domain, read_access_token)
+    account_info = mastodonTool.get_account_info(domain1, read_access_token1)
     params = {"exclude_replies": 1, "exclude_reblogs": 1}
-    filename = "{}@{}".format(account_info["username"], domain)
+    filename = "{}@{}".format(account_info["username"], domain1)
     filepath = os.path.join("./chainfiles", os.path.basename(filename.lower()) + ".json")
     if (os.path.isfile(filepath) and datetime.datetime.now().timestamp() - os.path.getmtime(filepath) < 60 * 60 * 24):
         print("モデルは再生成されません")
     else:
-        exportModel.generateAndExport(mastodonTool.loadMastodonAPI(domain, read_access_token, account_info['id'], params), filepath)
+        account_info1 = mastodonTool.get_account_info(domain1, read_access_token1)
+        exportModel.generateAndExport(mastodonTool.loadMastodonAPI(domain1, read_access_token1, account_info1['id'], params), filepath)
+        print("LOG,GENMODEL," + str(datetime.datetime.now()) + "," + account_info["username"].lower())   # Log
+        account_info2 = mastodonTool.get_account_info(domain2, read_access_token2)
+        exportModel.generateAndExport(mastodonTool.loadMastodonAPI(domain2, read_access_token2, account_info2['id'], params), filepath)
+        print("LOG,GENMODEL," + str(datetime.datetime.now()) + "," + account_info["username"].lower())   # Log
+        account_info3 = mastodonTool.get_account_info(domain3, read_access_token3)
+        exportModel.generateAndExport(mastodonTool.loadMastodonAPI(domain3, read_access_token3, account_info3['id'], params), filepath)
+        print("LOG,GENMODEL," + str(datetime.datetime.now()) + "," + account_info["username"].lower())   # Log
+        account_info4 = mastodonTool.get_account_info(domain4, read_access_token4)
+        exportModel.generateAndExport(mastodonTool.loadMastodonAPI(domain4, read_access_token4, account_info4['id'], params), filepath)
         print("LOG,GENMODEL," + str(datetime.datetime.now()) + "," + account_info["username"].lower())   # Log
     # 生成
     with open("./chainfiles/{}@{}.json".format(account_info["username"].lower(), domain)) as f:
